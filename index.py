5eb3737982b8215e246624c0a292720f38c05800
U-ASPIRE\Victor <Victor@aspire.cht.com.tw>
2020-03-21 20:10:33 +0800

    speed up by Python

--- index.py
+++ index.py
@@ -4,16 +4,31 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        z = []
         sign = 1
         if x < 0:
         	sign = -1
         	x *= -1
-        y = str(x) [::-1]
-        t = int(y) * sign
-        if t > pow(2, 31) - 1 or t < pow(2, 31) * -1:
+        y = 0
+        while x > 10:
+        	if y > 0:
+        		y *= 10
+        	y += x % 10
+        	x = x / 10
+        y *= 10
+        y += x % 10
+        if y > pow(2, 31) - 1 or y < pow(2, 31) * -1:
         	return 0
-        return t
+        return y*sign
+#        z = []
+#        sign = 1
+#        if x < 0:
+#        	sign = -1
+#        	x *= -1
+#        y = str(x) [::-1]
+#        t = int(y) * sign
+#        if t > pow(2, 31) - 1 or t < pow(2, 31) * -1:
+#        	return 0
+#        return t
 
 my_test = Solution()
 sample = [123, -123, 120, 1534236469]
